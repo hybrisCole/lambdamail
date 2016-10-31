a1a10634122f6967affc937c48f15cf0e525f2e2
Alberto Cole <acpii2005@gmail.com>
2016-10-31 15:55:49 -0600

    fix semaphore build

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-console.log('loading function')
-exports.handler = function(event, context) {
-    console.log('hello from semaphore');
-    console.log(event.key1);
-    console.log(event.key2);
-    console.log(event.key3);
-    context.succeed(event.key1);
+/* eslint-disable no-console */
+console.log('loading function');
+exports.handler = (event, context) => {
+  console.log('hello from semaphore');
+  console.log(event.key1);
+  console.log(event.key2);
+  console.log(event.key3);
+  context.succeed(event.key1);
 };
