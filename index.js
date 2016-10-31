878fb82ca7029318a275171d28d5e40ef86e360c
Alberto Cole <acpii2005@gmail.com>
2016-10-31 17:43:31 -0600

    mailer sample

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 /* eslint-disable no-console */
-console.log('loading function');
+const aws = require('aws-sdk');
+aws.config.update({
+  region          : 'us-east-1',
+  accessKeyId     : 'AKIAJMGM4SRPFNPVYT4Q',
+  secretAccessKey : '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',
+});
+const ses = new aws.SES({apiVersion : '2012-10-17'});
+
 exports.handler = function handler (event, context) {
-  console.log('hello from semaphore');
-  console.log(event.key1);
-  console.log(event.key2);
-  console.log(event.key3);
-  context.succeed(event.key1);
+  ses.sendEmail({
+    Source      : 'acpii2005@gmail.com',
+    Destination : { ToAddresses : 'acpii2005@gmail.com' },
+    Message     : {
+      Subject : {
+        Data : '4Tius App',
+      },
+      Body : {
+        Html : {
+          Data : `
+            <p>
+              HOLA
+            </p>
+          `,
+        },
+      },
+    },
+  }, (err, data) => {
+    if (err) {
+      context.succeed(err);
+    }
+    context.succeed(null, data);
+  });
 };
