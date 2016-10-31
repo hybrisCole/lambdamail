a52b27763a888891a2a2b00b91e858d6ecf7cb34
Alberto Cole <acpii2005@gmail.com>
2016-10-31 17:59:32 -0600

    weird node

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
+/* eslint-disable no-var */
 var aws = require('aws-sdk');
-
+var ses = new aws.SES({apiVersion : '2012-10-17'});
 aws.config.update({
   region          : 'us-east-1',
   accessKeyId     : 'AKIAJMGM4SRPFNPVYT4Q',
   secretAccessKey : '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',
 });
-var ses = new aws.SES({apiVersion : '2012-10-17'});
 
 /*
 const lambda = new aws.Lambda({apiVersion: '2015-03-31'});
